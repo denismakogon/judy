0ba5444b4a2678114aa4b6facfb6506b2ae9329f
Denys Makogon <denys.makogon@oracle.com>
2023-10-14 22:12:16 +0200

    trying to introduce per-arch files

---
+++ toolchains/mac-x86_64.cmake
@@ -0,0 +1,111 @@
+
+# Conan automatically generated toolchain file
+# DO NOT EDIT MANUALLY, it will be overwritten
+
+# Avoid including toolchain file several times (bad if appending to variables like
+#   CMAKE_CXX_FLAGS. See https://github.com/android/ndk/issues/323
+include_guard()
+
+MACRO(SUBDIRLIST result curdir)
+    FILE(GLOB children ${curdir}/*)
+    SET(dirlist "")
+    FOREACH(child ${children})
+        IF(IS_DIRECTORY ${child})
+            LIST(APPEND dirlist ${child})
+        ENDIF()
+    ENDFOREACH()
+    SET(${result} ${dirlist})
+ENDMACRO()
+
+message(STATUS "Using Conan toolchain: ${CMAKE_CURRENT_LIST_FILE}")
+
+if(${CMAKE_VERSION} VERSION_LESS "3.15")
+    message(FATAL_ERROR "The 'CMakeToolchain' generator only works with CMake >= 3.15")
+endif()
+
+
+
+
+
+
+
+
+
+
+# Set the architectures for which to build.
+set(CMAKE_OSX_ARCHITECTURES arm64 CACHE STRING "" FORCE)
+# Setting CMAKE_OSX_SYSROOT SDK, when using Xcode generator the name is enough
+# but full path is necessary for others
+set(CMAKE_OSX_SYSROOT macosx CACHE STRING "" FORCE)
+set(BITCODE "")
+set(FOBJC_ARC "")
+set(VISIBILITY "")
+#Check if Xcode generator is used, since that will handle these flags automagically
+if(CMAKE_GENERATOR MATCHES "Xcode")
+  message(DEBUG "Not setting any manual command-line buildflags, since Xcode is selected as generator.")
+else()
+    string(APPEND CONAN_C_FLAGS " ${BITCODE} ${FOBJC_ARC}")
+    string(APPEND CONAN_CXX_FLAGS " ${BITCODE} ${VISIBILITY} ${FOBJC_ARC}")
+endif()
+
+string(APPEND CONAN_CXX_FLAGS " -stdlib=libc++")
+
+
+message(STATUS "Conan toolchain: C++ Standard 17 with extensions ON")
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_EXTENSIONS ON)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+# Extra c, cxx, linkflags and defines
+
+
+if(DEFINED CONAN_CXX_FLAGS)
+  string(APPEND CMAKE_CXX_FLAGS_INIT " ${CONAN_CXX_FLAGS}")
+endif()
+if(DEFINED CONAN_C_FLAGS)
+  string(APPEND CMAKE_C_FLAGS_INIT " ${CONAN_C_FLAGS}")
+endif()
+if(DEFINED CONAN_SHARED_LINKER_FLAGS)
+  string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT " ${CONAN_SHARED_LINKER_FLAGS}")
+endif()
+if(DEFINED CONAN_EXE_LINKER_FLAGS)
+  string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT " ${CONAN_EXE_LINKER_FLAGS}")
+endif()
+
+get_property( _CMAKE_IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE )
+if(_CMAKE_IN_TRY_COMPILE)
+    message(STATUS "Running toolchain IN_TRY_COMPILE")
+    return()
+endif()
+
+set(CMAKE_FIND_PACKAGE_PREFER_CONFIG ON)
+
+# Definition of CMAKE_MODULE_PATH
+# the generators folder (where conan generates files, like this toolchain)
+list(PREPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+# Definition of CMAKE_PREFIX_PATH, CMAKE_XXXXX_PATH
+# The Conan local "generators" folder, where this toolchain is saved.
+list(PREPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_LIST_DIR} )
+SUBDIRLIST(SUBDIRS "$ENV{HOME}/.conan2/p/b/")
+FOREACH(subdir ${SUBDIRS})
+  list(PREPEND CMAKE_LIBRARY_PATH "${subdir}/p/lib")
+  list(PREPEND CMAKE_INCLUDE_PATH "${subdir}/p/include")
+ENDFOREACH()
+
+
+if (DEFINED ENV{PKG_CONFIG_PATH})
+set(ENV{PKG_CONFIG_PATH} "${CMAKE_CURRENT_LIST_DIR}:$ENV{PKG_CONFIG_PATH}")
+else()
+set(ENV{PKG_CONFIG_PATH} "${CMAKE_CURRENT_LIST_DIR}:")
+endif()
+
+
+
+
+# Variables
+# Variables  per configuration
+
+
+# Preprocessor definitions
+# Preprocessor definitions per configuration
