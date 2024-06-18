75e7c83641445e6e6b0643307c511ce51a6f7ad6
Denys Makogon <denys.makogon@oracle.com>
2024-06-18 18:50:51 +0200

    updating formatting

--- include/api.h
+++ include/api.h
@@ -3,10 +3,15 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    int nativeServer(void (*handler)(char* /* data */, long /* bytesReceived*/, char* /* host */, int /* port */), int port, int bufferSize, int threadCount);
+    int nativeServer(void (*handler)(char* /* data */, long /* bytesReceived*/, 
+                                        char* /* host */, int /* port */), 
+                                        int port, int bufferSize, int threadCount);
     void boostServer(void (*handler)(struct udp_request* request), 
-                     void (*before_handler)(char* /* data */, char* /* errorCategory */, int /* errorCode */, long /* bytesReceived */, char* /* client */, int /* port */), 
-                     void (*after_handler)(char* /* data */, long /* bytesReceived*/, char* /* host */, int /* port */), 
+                     void (*before_handler)(char* /* data */, char* /* errorCategory */, 
+                                            int /* errorCode */, long /* bytesReceived */, 
+                                            char* /* client */, int /* port */), 
+                     void (*after_handler)(char* /* data */, long /* bytesReceived*/, 
+                                            char* /* host */, int /* port */), 
                      int port, int bufferSize, int threadCount);
 #ifdef __cplusplus
 }
